6b091207c4a94ab0c200abd9ca164a74c41d83a6
Pedro Meira <petergamer8k@gmail.com>
2022-07-07 18:27:04 -0300

    Adding the login with JWT, and the the private route

--- src/credentials.exemple..ts
+++ src/credentials.exemple..ts
@@ -1,5 +1,8 @@
-export const credentials = {
+export const credentialsExemple = {
     cryptoJS: {
         SECRET_KEY: "", // Secret Passphrase, put here the secret key to use the cryptoJS
+    },
+    jwt:{
+        secret: "wertyiudfcbvnbmnhydfgsavbjkhadskghjvnbchjkvbnkj"
     }
 }
--- src/server.ts
+++ src/server.ts
@@ -4,15 +4,46 @@ import { decrypt } from './functions/decrypt';
 import { encrypt } from './functions/encrypt';
 import { findUserInDatabaseByEmail } from './functions/findUserInDatabaseByEmail';
 import { prisma } from './prisma';
+import jwt from 'jsonwebtoken';
+import { credentials } from './credentials';
+
+//express types
+import { Request, Response, NextFunction } from 'express';
 
 
 const PORT = 3333;
 
 const app = express();
+var pedro = 1;
 
+
+//Middleware
 app.use(express.json());
 
+function checkToken(req: Request, res: Response, next: NextFunction ) {
+    const authHeader = req.headers.authorization;
+    const token  = authHeader && authHeader.split(' ')[1];
 
+    if(!token) {
+        return res.status(401).json({error: true, msg: "Access denied"});
+    } 
+
+    try {
+
+        const secret = credentials.jwt.secret;
+
+        jwt.verify(token, secret); 
+        next(); 
+
+    }catch(error) {
+        console.log(error);
+        return res.status(401).json({error: true, msg:"Invalid token"});
+        
+    }
+}
+
+
+//Public route
 app.post('/user/register', async (req, res) => {
 
     type userProps = {
@@ -22,11 +53,11 @@ app.post('/user/register', async (req, res) => {
     }
 
     type userRegisteredProps = {
-		id: string,
-		email: string,
+        id: string,
+        email: string,
         password?: string
         name?: string
-	}
+    }
 
     const { email, password, name }: userProps = req.body.user;
 
@@ -49,7 +80,7 @@ app.post('/user/register', async (req, res) => {
     const encryptedPassword = encrypt(password);
 
 
-    const userRegistered: userRegisteredProps = await createUserInDataBase({ email, encryptedPassword, name});
+    const userRegistered: userRegisteredProps = await createUserInDataBase({ email, encryptedPassword, name });
 
     return res.status(201).json({ data: userRegistered });
 });
@@ -63,32 +94,66 @@ app.post('/user/login', async (req, res) => {
 
     const { password, email }: userProps = req.body.user;
 
-    if(!password) {
+    if (!password) {
         return res.status(422).json({ error: true, msg: "The password is required" });
     }
 
-    if(!email) {
+    if (!email) {
         return res.status(422).json({ error: true, msg: "The email is required" });
     }
 
     const userFound = await findUserInDatabaseByEmail(email);
 
-    if(!userFound) {
+    if (!userFound) {
         return res.status(404).json({ error: true, msg: "User or password invalid" });
     }
 
     const decryptPassword = decrypt(userFound.password);
 
-    if(password !== decryptPassword) {
+    if (password !== decryptPassword) {
         return res.status(404).json({ error: true, msg: "User or password invalid" });
     }
+    try {
+        const secret = credentials.jwt.secret;
+        const token = jwt.sign({
+            id: userFound.id,
+        }, secret);
 
-    return res.status(200).json({error: false, user: {email, password}});
-})
+        return res.status(200).json({ error: false, user: { email, token } });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ error: true, msg: "Internal server error, please try again later" });
+
+    }
+
+});
+
+//Private route
+
+//Get user data by it id
+app.get('/user/:id', checkToken, async (req: Request, res: Response) => {
+    const userId = req.params.id;
+    const userFound = await prisma.user.findFirst({
+        where: {
+            id: userId,
+        }, 
+        select: {
+            password: false,
+            email: true, 
+            id: true,
+            name: true
+        }
+    }); 
+
+    if (!userFound) {
+        return res.status(404).json({ error: true, msg: "User not found" })
+    }
+
+
+    return res.status(200).json({ error: false, user: userFound });
+});
 
 app.listen(PORT, () => {
     console.log("HTTP server running on the port " + PORT);
-    
-}); 
 
-
+});
