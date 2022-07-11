5b204e0795700f08a186fb9acc73b9f745f360df
Pedro Meira <petergamer8k@gmail.com>
2022-07-11 11:43:02 -0300

    the function to create a movie is done

--- src/functions/deleteMovieByTMDBidAndListId.ts
+++ src/functions/deleteMovieByTMDBidAndListId.ts
@@ -1,11 +1,41 @@
 import { prisma } from "../prisma";
 import { movieProps } from "./addMoviesToList";
 
-export function deleteMovieByTmdBidAndListId({ TMDBid, listId }: movieProps) {
+export async function deleteMovieByTmdBidAndListId({ TMDBid, listId }: movieProps) {
+
+    try {
+
+        const movieFound = await prisma.movie.findFirst({
+            where: {
+                TMDBid,
+                listId
+            }
+        });
+
+        if (!movieFound) {
+            return null;
+        }
+
+        const movieId = movieFound.id;
+
+
+        const movieDeleted = await prisma.movie.delete({
+            where: {
+                id: movieId,
+            }
+        });
+
+        return movieDeleted;
+    } catch (error) {
+        console.error(error);
+        return null;
+
+    }
 
 }
 
 /*
+Primeiro buscar o id do filme que deseja ser deletado
 Deletar um filme da lista de um usuário especifico pelo ID da lista e do TMDB
 
 */
