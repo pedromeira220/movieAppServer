90cbc821ee4ef377c22562b56c23dc2b30d0b322
Pedro Meira <petergamer8k@gmail.com>
2022-07-11 11:27:55 -0300

    starting creating the file of the function

---
+++ src/functions/deleteMovieByTMDBidAndListId.ts
@@ -0,0 +1,11 @@
+import { prisma } from "../prisma";
+import { movieProps } from "./addMoviesToList";
+
+export function deleteMovieByTmdBidAndListId({ TMDBid, listId }: movieProps) {
+
+}
+
+/*
+Deletar um filme da lista de um usuário especifico pelo ID da lista e do TMDB
+
+*/
