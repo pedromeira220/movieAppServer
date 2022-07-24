58ff653ff70dc5cbd04db506c25e632ce696b5a7
Pedro Meira <petergamer8k@gmail.com>
2022-07-24 20:09:11 -0300

    Removing the createntials file of the git ignore

---
+++ src/credentials.ts
@@ -0,0 +1,8 @@
+export const credentials = {
+    cryptoJS: {
+        SECRET_KEY: "chaveultramentesecreta12354321"
+    },
+    jwt:{
+        secret: "wertyiudfcbvnbmnhydfgsavbjkhadskghjvnbchjkvbnkj"
+    }
+}
